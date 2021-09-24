6ab348c993a1a2da4ab2a370da39de6f1b8664f8
Kettrin62 <KettyKazakova@yandex.ru>
2021-09-24 11:04:49 +0300

    feat: сделать блок О проекте

--- scripts/index.js
+++ scripts/index.js
@@ -8,23 +8,26 @@ const videoContainer = bodyElement.querySelector('.video__iframes');
 const videoElements = bodyElement.querySelectorAll('.video__iframe');
 
 
+function changVideoBlock() {
+  if (videoElements.length === 1) {
+    titleVideoElement.classList.add('video__title_number_one');
+    videoContainer.classList.add('video__iframes_number_one');
+    videoElements.forEach((element) => {
+      element.classList.add('video__iframe_number_one');
+    })
+  } else if (videoElements.length === 2) {
+    titleVideoElement.classList.add('video__title_number_two');
+    videoContainer.classList.add('video__iframes_number_two');
+    videoElements.forEach((element) => {
+      element.classList.add('video__iframe_number_two');
+    })
+  } else if (videoElements.length === 3) {
+    titleVideoElement.classList.add('video__title_number_three');
+    videoContainer.classList.add('video__iframes_number_three');
+    videoElements.forEach((element) => {
+      element.classList.add('video__iframe_number_three');
+    })
+  };
+}
 
-if (videoElements.length === 1) {
-  titleVideoElement.classList.add('video__title_number_one');
-  videoContainer.classList.add('video__iframes_number_one');
-  videoElements.forEach((element) => {
-    element.classList.add('video__iframe_number_one');
-  })
-} else if (videoElements.length === 2) {
-  titleVideoElement.classList.add('video__title_number_two');
-  videoContainer.classList.add('video__iframes_number_two');
-  videoElements.forEach((element) => {
-    element.classList.add('video__iframe_number_two');
-  })
-} else if (videoElements.length === 3) {
-  titleVideoElement.classList.add('video__title_number_three');
-  videoContainer.classList.add('video__iframes_number_three');
-  videoElements.forEach((element) => {
-    element.classList.add('video__iframe_number_three');
-  })
-};
+changVideoBlock();
